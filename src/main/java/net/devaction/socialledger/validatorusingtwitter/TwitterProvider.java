016925ec2977d6d49e0f2016561a36342d0e3d0f
VictorGil <victorgilsanz@gmail.com>
2018-03-18 18:03:07 +0100

    Making TwitterProvider a singleton for convenience

--- src/main/java/net/devaction/socialledger/validatorusingtwitter/TwitterProvider.java
+++ src/main/java/net/devaction/socialledger/validatorusingtwitter/TwitterProvider.java
@@ -3,7 +3,9 @@ package net.devaction.socialledger.validatorusingtwitter;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import net.devaction.socialledger.validatorusingtwitter.key.DecryptedKeyPairProvider;
 import net.devaction.socialledger.validatorusingtwitter.key.KeyPair;
+import net.devaction.socialledger.validatorusingtwitter.token.DecryptedTokenPairProvider;
 import net.devaction.socialledger.validatorusingtwitter.token.TokenPair;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
@@ -14,11 +16,62 @@ import twitter4j.auth.AccessToken;
  * 
  * since Fri 2018-Mar-16 
  */
-public class TwitterProvider {
+public class TwitterProvider{
     private static final Log log = LogFactory.getLog(TwitterProvider.class);
     
-   
-    public static Twitter provide(String consumerApiKey, String consumerApiSecret, 
+    private static TwitterProvider INSTANCE;
+    
+    private final KeyPair keyPair;
+    private final TokenPair tokenPair;
+    
+    public TwitterProvider getInstance(){
+        if (INSTANCE == null)
+            INSTANCE = new TwitterProvider();
+        return INSTANCE;
+    }
+        
+    private TwitterProvider(){        
+        keyPair = constructDecryptedKeyPairProvider().provide();
+        tokenPair = constructDecryptedTokenPairProvider().provide();        
+    }
+    
+    public Twitter provide(){
+        return provide(keyPair, tokenPair);
+    }
+    
+    static DecryptedKeyPairProvider constructDecryptedKeyPairProvider(){
+        GlobalProperties properties = GlobalProperties.getInstance();
+        
+        DecryptedKeyPairProvider decryptedKeyPairProvider;
+        if (properties.TWITTER_CONSUMER_API_KEY == null){
+            decryptedKeyPairProvider = new DecryptedKeyPairProvider(
+                    properties.TWITTER_CONSUMER_API_KEY_ENCRYPTED, properties.TWITTER_CONSUMER_API_SECRET_ENCRYPTED,
+                    properties.DECRYPT_PASSWORD_ENV_VAR_NAME);
+        } else{            
+            decryptedKeyPairProvider = new DecryptedKeyPairProvider(
+                    properties.TWITTER_CONSUMER_API_KEY, properties.TWITTER_CONSUMER_API_SECRET); 
+        }
+        
+        return decryptedKeyPairProvider;
+    }
+    
+    static DecryptedTokenPairProvider constructDecryptedTokenPairProvider(){
+        GlobalProperties properties = GlobalProperties.getInstance();
+        
+        DecryptedTokenPairProvider decryptedTokenPairProvider;
+        if (properties.TWITTER_ACCESS_TOKEN == null) {
+            decryptedTokenPairProvider = new DecryptedTokenPairProvider(
+                    properties.TWITTER_ACCESS_TOKEN_ENCRYPTED, properties.TWITTER_ACCESS_TOKEN_SECRET_ENCRYPTED,
+                    properties.DECRYPT_PASSWORD_ENV_VAR_NAME);
+        } else{
+            decryptedTokenPairProvider = new DecryptedTokenPairProvider(
+                    properties.TWITTER_ACCESS_TOKEN, properties.TWITTER_ACCESS_TOKEN_SECRET);
+        }
+        
+        return decryptedTokenPairProvider;
+    }
+    
+    static Twitter provide(String consumerApiKey, String consumerApiSecret, 
             String accessTokenString, String accessTokenSecretString){
         
         Twitter twitter = TwitterFactory.getSingleton();
@@ -36,5 +89,3 @@ public class TwitterProvider {
                 tokenPair.getAccesToken(), tokenPair.getAccessTokenSecret());
     }
 }
-
-
